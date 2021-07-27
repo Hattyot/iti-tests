c1c5591fab9497cfe0d2e334c07f98afd3276196
hattyot <hattyot@gmail.com>
2021-07-27 22:08:39 +0300

    group

--- tests/group/part1_test.py
+++ tests/group/part1_test.py
@@ -6,6 +6,4 @@ def test_dummy_test_always_passes():
 
 
 def test_dummy_test_always_fails():
-    with open('part1_test.py') as f:
-        raise Exception(f.read())
-    assert True
+    assert False
