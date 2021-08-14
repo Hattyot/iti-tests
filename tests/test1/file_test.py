b8bf2466eaa7121ad778b990355c95ed3efd1942
hattyot <hattyot@gmail.com>
2021-08-14 16:15:09 +0300

    ????

--- tests/test1/file_test.py
+++ tests/test1/file_test.py
@@ -8,7 +8,7 @@ def test_dummy_test_always_passes():
 def test_dummy_test_always_fails():
     assert False
 
-
-@pytest.mark.skip()
-def test_dummy_test_always_skip():
-    assert False
+#
+# @pytest.mark.skip()
+# def test_dummy_test_always_skip():
+#     assert False
