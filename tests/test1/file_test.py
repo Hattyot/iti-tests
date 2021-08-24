c0eb34d0653cb0007b26b4c338859b8116b106da
hattyot <hattyot@gmail.com>
2021-08-24 16:46:33 +0300

    dsa

--- tests/test1/file_test.py
+++ tests/test1/file_test.py
@@ -4,39 +4,5 @@ import file
 
 
 def test_dummy_test_always_passes():
-    # print(test)
-    raise Exception(test)
+    assert True
 
-
-def test_dummy_test_always_fails():
-    a = 1 + 1
-    b = a + 1
-    c = b + 1
-    assert c == 100
-
-def test_dummy_test_always_fails2():
-    a = 1 + 1
-    b = a + 1
-    c = b + 1
-    assert c == 100, 'assert error message'
-
-
-def test_dummy_test_always_fails3():
-    raise Exception('exception message')
-
-
-def test_dummy_test_always_fails4():
-    raise KeyError('keyerror exception message')
-
-
-def test_dummy_test_always_fails5():
-    pytest.fail()
-
-
-def test_dummy_test_always_fails6():
-    pytest.fail("pytest fail message")
-
-#
-@pytest.mark.skip()
-def test_dummy_test_always_skip():
-    assert False
